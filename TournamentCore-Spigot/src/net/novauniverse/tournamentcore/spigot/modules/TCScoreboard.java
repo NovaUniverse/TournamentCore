4042e68ad6c2cf8f963067dd109f685d0d3b5bee
Anton <udenanton@hotmail.com>
2021-02-11 11:52:31 +0100

    Chagnged web ui AND FINALLY FIXED THE STARTING IN 00:01 BUG

--- TournamentCore-Spigot/src/net/novauniverse/tournamentcore/spigot/modules/TCScoreboard.java
+++ TournamentCore-Spigot/src/net/novauniverse/tournamentcore/spigot/modules/TCScoreboard.java
@@ -10,6 +10,7 @@ import net.novauniverse.tournamentcore.spigot.TournamentCore;
 import net.novauniverse.tournamentcore.spigot.score.ScoreManager;
 import net.novauniverse.tournamentcore.team.TournamentCoreTeam;
 import net.novauniverse.tournamentcore.team.TournamentCoreTeamManager;
+import net.zeeraa.novacore.commons.log.Log;
 import net.zeeraa.novacore.commons.utils.TextUtils;
 import net.zeeraa.novacore.spigot.NovaCore;
 import net.zeeraa.novacore.spigot.language.LanguageManager;
@@ -62,32 +63,24 @@ public class TCScoreboard extends NovaModule implements Listener {
 
 						NetherBoardScoreboard.getInstance().setPlayerLine(12, player, ChatColor.GOLD + LanguageManager.getString(player, "tournamentcore.scoreboard.your_ping") + formatPing(ping) + "ms " + (ping > 800 ? ChatColor.YELLOW + TextUtils.ICON_WARNING : ""));
 
-						if (GameManager.getInstance().getCountdown().isCountdownRunning()) {
-							gameCountdownShown = true;
-							// NetherBoardScoreboard.getInstance().setGlobalLine(COUNTDOWN_LINE,
-							// ChatColor.GOLD+ LanguageManager.getString(player, "")+ ChatColor.AQUA +
-							// TextUtils.secondsToHoursMinutes(GameManager.getInstance().getCountdown().getTimeLeft()));
-							
-							//Log.trace("Särbarn(TM)", "showing countdown");
-							
-							NetherBoardScoreboard.getInstance().setPlayerLine(COUNTDOWN_LINE, player, ChatColor.GOLD + LanguageManager.getString(player, "tournamentcore.scoreboard.starting_in") + ChatColor.AQUA + TextUtils.secondsToHoursMinutes(GameManager.getInstance().getCountdown().getTimeLeft()));
-						} else {
-							if (gameCountdownShown) {
-								gameCountdownShown = false;
-								NetherBoardScoreboard.getInstance().clearPlayerLine(COUNTDOWN_LINE, player);
-
-								new BukkitRunnable() {
-									@Override
-									public void run() {
-										NetherBoardScoreboard.getInstance().clearPlayerLine(COUNTDOWN_LINE, player);
-									}
-								}.runTaskLater(TournamentCore.getInstance(), 20L);
-							}
-						}
-
 						if (recentTps.length > 0) {
 							double tps = recentTps[0];
 							NetherBoardScoreboard.getInstance().setPlayerLine(13, player, ChatColor.GOLD + LanguageManager.getString(player, "tournamentcore.scoreboard.average_tps") + formatTps(tps) + (tps < 18 ? " " + ChatColor.RED + TextUtils.ICON_WARNING : ""));
+						}
+					}
+
+					if (GameManager.getInstance().getCountdown().isCountdownRunning()) {
+						gameCountdownShown = true;
+
+						for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+							NetherBoardScoreboard.getInstance().setPlayerLine(COUNTDOWN_LINE, player, ChatColor.GOLD + LanguageManager.getString(player, "tournamentcore.scoreboard.starting_in") + ChatColor.AQUA + TextUtils.secondsToHoursMinutes(GameManager.getInstance().getCountdown().getTimeLeft()));
+						}
+					} else {
+						if (gameCountdownShown) {
+							gameCountdownShown = false;
+							for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+								NetherBoardScoreboard.getInstance().clearPlayerLine(COUNTDOWN_LINE, player);
+							}
 						}
 					}
 				}
